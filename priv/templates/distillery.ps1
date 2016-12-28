94dcfa9126407955f8fac64f576cf3b5630e5e2a
Onorio Catenacci <catenacci@ieee.org>
2016-12-27 21:26:37 -0500

    Beginning to add unit tests
    Beginning to add unit tests for the distillery scripts as well as starting to add utility functions.

--- priv/templates/distillery.ps1
+++ priv/templates/distillery.ps1
@@ -8,4 +8,22 @@ Param(
    [System.String]$Command
 )
 
+function Get-ServiceByName
+{
+    param([string]$servicename)
+    try
+    {
+	$svc = Get-Service -Name $servicename -ErrorAction SilentlyContinue
+	$svc -ne $null
+    }
+    catch{$false}
+}
+
+function Get-BinaryByName
+{
+    param([string]$binaryname)
+    $bin = Get-Command $binaryname -ErrorAction SilentlyContinue
+}
+  
+        
 write-host "Command: $Command"
