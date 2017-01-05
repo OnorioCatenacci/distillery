c9b1c4a00366857b1b7770026c6ae4467f5d8b1c
Onorio Catenacci <catenacci@ieee.org>
2017-01-04 19:55:02 -0500

    Modifying Binary Search
    Modifying the binary search to first check the path and then if the binary isn't found in the path check under Program Files

--- priv/templates/distillery.ps1
+++ priv/templates/distillery.ps1
@@ -24,6 +24,7 @@ function Test-ServiceIsInstalled
 function Get-BinaryInPath
 {
     param([string]$binaryname)
+
     $bin = Get-Command $binaryname -ErrorAction SilentlyContinue
 
     if($bin -ne $null)
