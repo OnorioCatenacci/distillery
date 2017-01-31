713197b39559762cef12d69ac487b4e9696dda42
Onorio Catenacci <catenacci@ieee.org>
2017-01-30 20:55:38 -0500

    Adding variables
    Started porting variables from the old batch file as well as beginning
    to figure out how to get the root directory for the scripts.  Next step
    is to write a powershell function to deal with directories with spaces.

--- priv/templates/distillery.ps1
+++ priv/templates/distillery.ps1
@@ -9,6 +9,58 @@ Param(
 )
 
 Set-Variable binaryNotFound -option Constant -value "No binary found"
+Set-Variable rel_name -option Constant -value "<%= rel_name %>"
+Set-Variable rel_vsn -option Constant -value "<%= rel_vsn %>"
+Set-Variable erts_vsn -option Constant -value "<%= erts_vsn %>"
+Set-Variable erl_opts  -option Constant -value "<%= erl_opts %>"
+
+function Fix-PathsWithSpaces
+{
+    param([string] $pathToFix)
+    $pathParts = $pathToFix.Split([System.IO.Path]::DirectorySeparatorChar)
+    #for each element in array if it contains a space, surround it with double quotes
+}
+
+function Set-ReleaseRoot
+{
+    $script_dir = $PSScriptRoot
+    $script_dir
+}
+
+function Find-ErtsDir
+{
+    $release_root = Set-ReleaseRoot
+    $possible_erts_dir = "$release_root\erts-$erts_vsn"
+    Write-Host "$possible_erts_dir"
+
+}
+<#@set possible_erts_dir=%release_root_dir%\erts-%erts_vsn%
+@if exist "%possible_erts_dir%" (
+  call :set_erts_dir_from_default
+) else (
+  call :set_erts_dir_from_erl
+)
+@goto :eof
+
+:: Set the ERTS dir from the passed in erts_vsn
+:set_erts_dir_from_default
+@set erts_dir=%possible_erts_dir%
+@set rootdir=%release_root_dir%
+@goto :eof
+
+:: Set the ERTS dir from erl
+:set_erts_dir_from_erl
+@for /f "delims=" %%i in ('where erl') do @(
+  set erl=%%i
+)
+@set dir_cmd="%erl%" -noshell -eval "io:format(\"~s\", [filename:nativename(code:root_dir())])." -s init stop
+@for /f %%i in ('%%dir_cmd%%') do @(
+  set erl_root=%%i
+)
+@set erts_dir=%erl_root%\erts-%erts_vsn%
+@set rootdir=%erl_root%
+@goto :eof #>
+
 
 function Test-ServiceIsInstalled
 {
