d32d83884adc44ce49880818292890b9c6301fbc
Onorio Catenacci <catenacci@ieee.org>
2017-02-06 12:50:40 -0500

    Removing blind code alleys
    Removing several code files that were produced in earlier attempts to figure how to create the powershell script properly.

--- priv/templates/boot_loader_win.ps1
+++
@@ -1,9 +0,0 @@
-<# Discover the current release directory from the directory
-   of this script and the start_erl.data file
-#>
-$script_dir=$PSScriptRoot
-$release_root_dir = Split-Path $script_dir -Parent
-
-$rel_vsn="<%= rel_vsn %>"
-$boot_script="$release_root_dir\releases\$rel_vsn\boot_win.ps1"
-iex $boot_script $args[0]
